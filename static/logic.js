165e2d183210360a8d75435d7f8b866e0b845c05
Robbie Hockley <robbie.hockley@gmail.com>
2022-10-24 19:50:34 +0800

    moved files for access

--- static/logic.js
+++ static/logic.js
@@ -1,39 +1,32 @@
 
 
-let map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
     center: [121.6, -24.4], // starting position [lng, lat]
     zoom: 3.6, // starting zoom
+    projection: 'globe'
     });
    
-// map.on('style.load', () => {    
-//     map.addSource('mine-data', {
-//         type: 'geojson',
-//         data: 'resources/operating_mines.json'
-// });
+map.on('load', () => {
+    map.addSource('mines', {
+        type: 'geojson',
+        data: 'https://github.com/ParkaviMathi/Project_3/blob/main/resources/clean_mines.geojson'
+    });
 
-// map.addlayer({
-//     'id': 'mines',
-//     'type': 'circle'
-//     'source': 'mine-data',
-//     'paint': {
-//         'circle-radius': 4,
-//         'circle-color': 'red'
-//     }
-    
-// })
+    map.on('style.load', () => {
+        map.setFog({}); // enable atmosphere and stars
+        });
 
-// // function createMarkers(response) {
-// //     let mines = response.data.MINE;
-// //     let mineMarkers = [];
-
-// //     for (let index = 0; index <MINE.length; index++) {
-// //         let mine = mines[index];
-
-// //         let mineMarker = L.marker([MINE.LATITUDE, MINE.LONGITUDE])
-// //             .bindPopup("<h3>" + MINE.SHORT_TITLE + "<h3><h3>Commodity: " + MINE.TARGET_GROUP_NAME + "</h3>");
-
-// //         mineMarkers.push(mineMarker);
-// //     }
-// // }
+    map.addLayer({
+        'id': 'mines-layer',
+        'type': 'circle',
+        'source': 'mines',
+        'paint': {
+            'circle-radius': 4,
+            'circle-stroke-width': 2,
+            'circle-color': 'red',
+            'circle-stroke-color': 'white'
+        }
+    });
+});
